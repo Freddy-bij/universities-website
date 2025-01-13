3a8f5fcbb5f3a7a5e6b6b2ed4d32d0de87b00bd9
Freddy Bijanja <freddybijanja31@gmail.com>
2025-01-13 14:07:05 +0200

    project setup

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const Student = () => {
   return (
@@ -6,11 +7,11 @@ const Student = () => {
     <div className="bg-[url('/public/team.png')] w-full h-screen flex flex-col justify-center  ">
        <div className="w-1/2 mx-auto ">
 
-       <div className="uppercase text-5xl font-bold mt-7">
+       <div className="uppercase text-4xl font-bold mt-7">
             <h1 className="pb-4">Preparing Students</h1>
             <h2 className="pb-4">for Successful Futurs</h2>
              </div>
-           <a href="/countries"><button className="bg-white text-black font-bold py-5 px-5 hover:bg-blue-200 hover:text-white hover:font-bold rounded-xl uppercase text-2xl mt-8">find your favorite country</button></a> 
+           <Link to="/countries"><button className="bg-white text-black font-bold py-5 px-5 hover:bg-blue-200 hover:text-white hover:font-bold rounded-xl uppercase text-2xl mt-8">find your favorite country</button></Link> 
        </div>
       
     </div>
